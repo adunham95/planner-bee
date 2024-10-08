5bcd719acb00717cc0e4732ba87548a488194d3b
Adrian <adunham95@gmail.com>
2024-10-08 13:39:35 -0400

    Added Logos

---
+++ src/lib/component/navlink.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { page } from '$app/stores';
+	// import Icon from '$lib/Icon.svelte';
+	export let icon;
+	export let href;
+	export let name;
+</script>
+
+<a
+	{href}
+	class:active={$page.url.pathname.includes(href)}
+	class="group flex gap-x-3 rounded-md bg-gray-50 p-2 text-sm font-semibold leading-6 text-brand-600"
+>
+	<svg
+		class="h-6 w-6 shrink-0 text-brand-600"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		aria-hidden="true"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+		/>
+	</svg>
+	Dashboard
+	<!-- <Icon {title} /> -->
+	{name}
+</a>
+
+<style lang="postcss">
+	a.active {
+		@apply bg-gray-50 text-brand-600;
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					50: '#f7fafc',
+					100: '#f2f7fc',
+					200: '#dce8f7',
+					300: '#c9d8f5',
+					400: '#a2b2eb',
+					500: '#7c85e3',
+					600: '#666fcc',
+					700: '#464dab',
+					800: '#2d3387',
+					900: '#1a1f66',
+					950: '#0b0d42'
+				},
+				'brand-alt': {
+					50: '#fcf7fc',
+					100: '#f8f0fa',
+					200: '#ead5f0',
+					300: '#dcbce6',
+					400: '#c192d4',
+					500: '#a56cc1',
+					600: '#8e57ad',
+					700: '#6e3d91',
+					800: '#512875',
+					900: '#351657',
+					950: '#1d0938'
+				}
+			}
+		}
+	},
+	plugins: []
+};
