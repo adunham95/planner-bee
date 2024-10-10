1d669a0e2979363c9a59f11c7dfd81c9a64ec592
Adrian <adunham95@gmail.com>
2024-10-10 14:53:08 -0400

    Working on order page

--- src/routes/ecard/[sku]/details/+page.svelte
+++ src/routes/ecard/[sku]/details/+page.svelte
@@ -180,12 +180,19 @@
 						/>
 					</div>
 
-					<button
+					<a
+						href="/order"
+						class="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					>
+						Send Card
+					</a>
+
+					<!-- <button
 						type="submit"
 						class="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 					>
 						Send Card
-					</button>
+					</button> -->
 				</form>
 
 				<!-- Product details -->
