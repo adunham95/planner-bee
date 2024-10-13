7f3958fcb8037d0134d4555e9b79bb77d1149333
Adrian <adunham95@gmail.com>
2024-10-13 13:33:12 -0400

    Added Customization

---
+++ prisma/migrations/20241013173242_remove_key/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `key` on the `ECardComponentOption` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ECardComponentOption" DROP COLUMN "key";
