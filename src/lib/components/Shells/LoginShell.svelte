f9c34c9a511169d7b1f9cafad9953d67b01d9527
Adrian <adunham95@gmail.com>
2024-10-18 10:18:54 -0400

    Working on sign up page

---
+++ src/lib/components/Shells/LoginShell.svelte
@@ -0,0 +1,8 @@
+<div class="flex min-h-full">
+	<div class="flex flex-1 flex-col justify-center px-4 py-12">
+		<slot />
+	</div>
+	<div class="relative hidden w-0 flex-1 lg:block">
+		<img class="absolute inset-0 h-full w-full object-cover" src="/images/outdoor.jpg" alt="" />
+	</div>
+</div>
