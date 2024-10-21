9a45de9445a1ebe8dc2ab9778a4787270a83badc
Adrian <adunham95@gmail.com>
2024-10-21 15:51:02 -0400

    Added cart link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -115,7 +115,6 @@
 
 					<div class="flex flex-1 items-center justify-end">
 						<!-- Account -->
-
 						{#if data?.user}
 							<a href="/dashboard" class="p-2 text-gray-400 hover:text-gray-500 lg:ml-4">
 								<span class="sr-only">Account</span>
@@ -146,6 +145,28 @@
 								</a>
 							</div>
 						{/if}
+
+						<!-- CART -->
+						<div class="ml-4 flow-root lg:ml-6">
+							<a href="/cart" class="group -m-2 flex items-center p-2">
+								<svg
+									class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									aria-hidden="true"
+									data-slot="icon"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+									/>
+								</svg>
+								<span class="sr-only">View Cart</span>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
