99f4412056ea6e4a83f0daf4c07289105adcea31
Adrian <adunham95@gmail.com>
2024-10-29 21:45:06 -0400

    Added max width

--- src/lib/components/EcardComponents/Message.svelte
+++ src/lib/components/EcardComponents/Message.svelte
@@ -9,7 +9,7 @@
 
 <p
 	{style}
-	class="mt-1 text-xl @screen-sm/ecard:text-[1.75rem] @screen-md/ecard:text-[2.5rem] @screen-lg/ecard:text-[3rem] @screen-ulra/ecard:text-[4rem] @screen-sm/ecard:text-center text-custom-600 px-2 py-3"
+	class="mt-1 text-xl @screen-sm/ecard:text-[1.75rem] @screen-md/ecard:text-[2.5rem] @screen-lg/ecard:text-[3rem] @screen-ulra/ecard:text-[4rem] @screen-sm/ecard:text-center text-custom-600 px-2 py-3 mx-auto max-w-xl"
 >
 	{value}
 </p>
