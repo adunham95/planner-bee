06328503bf9e013a2c08cb1604e24feeb7da8be7
Adrian <adunham95@gmail.com>
2024-11-04 20:02:05 -0500

    Made admin wrapper

---
+++ src/routes/(admin)/admin/ecards/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Container from '$lib/components/Containers/container.svelte';
+	import PageHeaderShell from '$lib/components/Shells/PageHeaderShell.svelte';
+	import Table from '$lib/components/Table/Table.svelte';
+	import TableCell from '$lib/components/Table/TableCell.svelte';
+	import TableRow from '$lib/components/Table/TableRow.svelte';
+
+	let { data } = $props();
+	console.log(data);
+</script>
+
+<PageHeaderShell title="eCard Products">
+	<Container>
+		<Table headers={['SKU', 'Name', 'Components']}>
+			{#each data.eCards as ecard}
+				<TableRow>
+					<TableCell>{ecard.sku}</TableCell>
+					<TableCell>{ecard.name}</TableCell>
+					<TableCell>{ecard.components.length}</TableCell>
+					<TableCell><a href="/admin/ecards/{ecard.sku}">Edit</a></TableCell>
+				</TableRow>
+			{/each}
+		</Table>
+	</Container>
+</PageHeaderShell>
---
+++ src/routes/(admin)/admin/ecards/new/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { run } from 'svelte/legacy';
+
+	import { enhance } from '$app/forms';
+	import ComponentList from '$lib/components/ComponentList.svelte';
+	import ECard from '$lib/components/ECard.svelte';
+	import TextArea from '$lib/components/Inputs/TextArea.svelte';
+	import TextInput from '$lib/components/Inputs/TextInput.svelte';
+	import PageHeaderShell from '$lib/components/Shells/PageHeaderShell.svelte';
+
+	let { data } = $props();
+	console.log(data);
+
+	let components: { id: string; value: any; action?: string }[] = $state([]);
+	run(() => {
+		components;
+	});
+
+	run(() => {
+		console.log({ components });
+	});
+</script>
+
+<PageHeaderShell title="New eCard">
+	<div class="flex">
+		<div class=" w-full max-w-[700px] p-2 m-2">
+			<h2 class="text-base font-semibold leading-6 text-gray-900">eCard Preview</h2>
+			<div class="card p-0">
+				<ECard
+					style="min-height: 300px"
+					components={components.map((c) => {
+						return {
+							id: c?.id || '',
+							ecardComponentID: c?.value?.component,
+							style: c?.value?.style,
+							value: c?.value?.default
+						};
+					})}
+				/>
+			</div>
+		</div>
+		<form method="POST" use:enhance class="w-full grid grid-cols-1 sm:grid-cols-2 gap-4">
+			<TextInput label="Name" id="name" showLabel />
+			<TextInput label="SKU" id="sku" showLabel />
+			<TextInput type="number" min="0" label="Price" id="price" showLabel />
+			<TextInput min="0" label="Image URL" id="imageURL" showLabel />
+			<TextArea label="Description" id="description" showLabel class="col-span-2" />
+			<h3 class="text-2xl">Components</h3>
+			<ComponentList
+				groupName="component"
+				class="col-span-2"
+				components={data.eCardComponents.map((c) => ({
+					id: c.id,
+					label: c.name
+				}))}
+				bind:value={components}
+			/>
+			<div>
+				<button type="submit">Save</button>
+			</div>
+		</form>
+	</div>
+</PageHeaderShell>
