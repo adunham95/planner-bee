1c85131aaef893eebbb1601063ee55fc84876e2d
Adrian <adunham95@gmail.com>
2025-03-31 11:08:09 -0400

    Add preferences

--- src/routes/(shop)/shop/ecard/preferences/+page.server.ts
+++ src/routes/(shop)/shop/ecard/preferences/+page.server.ts
@@ -2,6 +2,8 @@ import prisma from '$lib/prisma';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 import { redirect } from '@sveltejs/kit';
+import type { Actions } from '../$types';
+import { logAllFormData } from '$lib/utils/logAllFormData';
 
 export const load: PageServerLoad = async ({ cookies, locals }) => {
 	const eCardID = cookies.get('eCard');
@@ -39,4 +41,40 @@ export const load: PageServerLoad = async ({ cookies, locals }) => {
 		eCard,
 		contacts
 	};
+};
+
+export const actions: Actions = {
+	addToCart: async ({ request, cookies }) => {
+		const data = await request.formData();
+		logAllFormData(data);
+
+		const cartOptions = [];
+
+		for (const pair of data.entries()) {
+			const [key, value] = pair;
+
+			cartOptions.push({
+				key,
+				value: value.toString()
+			});
+			console.log(pair[0], pair[1]);
+		}
+
+		const cartID = cookies.get('cart');
+		const eCardId = cookies.get('eCard');
+
+		console.log('cartID', cartID);
+
+		const ecardOptions = await prisma.optionItem.createMany({
+			data: cartOptions.map((opt) => ({
+				orderID: cartID,
+				eCardId,
+				...opt
+			}))
+		});
+
+		console.log({ ecardOptions });
+
+		redirect(303, '/shop/cart');
+	}
 };
--- src/routes/(shop)/shop/ecard/preferences/+page.svelte
+++ src/routes/(shop)/shop/ecard/preferences/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import Container from '$lib/components/Containers/container.svelte';
 	import CheckCards from '$lib/components/Inputs/CheckCards.svelte';
 	import DisplayTitle from '../../../components/DisplayTitle.svelte';
@@ -14,76 +15,78 @@
 	price={data.eCard.eCardTemplate?.cost}
 	description={data.eCard.eCardTemplate?.description}
 	formName="add-to-cart"
-	buttonText="Continue"
+	buttonText="Add To Cart"
 />
 
 <Container className="py-8">
-	<div>
-		<DisplayTitle title="Set & Send" />
-		<CheckCards
-			className="grid grid-cols-4"
-			type="radio"
-			hideCheck
-			passValue
-			bind:checkedOptions={subscriptionType}
-			groupName="sendSendType"
-			options={[
-				{
-					id: 'single',
-					title: 'Single',
-					description: 'Send a single card, one time'
-				},
-				{
-					id: 'schedule',
-					title: 'Schedule',
-					description: 'Send a recurring order, on a set schedule'
-				},
-				{
-					id: 'event',
-					title: 'Event Based',
-					description: 'Send a eCard based on an contacts event, birthday, anniversary, etc'
-				}
-			]}
-		/>
-	</div>
-	<div>
-		<DisplayTitle title="Set Timeline" />
-		<CheckCards
-			className="grid grid-cols-4"
-			type="radio"
-			hideCheck
-			passValue
-			groupName="sendSendTime"
-			options={[
-				{
-					id: 'now',
-					title: 'Send Now',
-					description: 'Send your eCard now',
-					checked: true
-				},
-				{
-					id: 'later',
-					title: 'Send Later',
-					description: 'Schedule your eCard for delivery'
-				}
-			]}
-		/>
-	</div>
-	<div>
-		<DisplayTitle title="Recipients" />
-		<CheckCards
-			className="grid grid-cols-4"
-			type="checkbox"
-			hideCheck
-			passValue
-			groupName="recipients"
-			options={data.contacts.map((c) => {
-				return {
-					id: c.id,
-					title: `${c.firstName} ${c.lastName}`,
-					description: `${c.email} | ${c.phone}`
-				};
-			})}
-		/>
-	</div>
+	<form method="POST" id="add-to-cart" action="?/addToCart" use:enhance>
+		<div>
+			<DisplayTitle title="Set & Send" />
+			<CheckCards
+				className="grid grid-cols-4"
+				type="radio"
+				hideCheck
+				passValue
+				bind:checkedOptions={subscriptionType}
+				groupName="sendSendType"
+				options={[
+					{
+						id: 'single',
+						title: 'Single',
+						description: 'Send a single card, one time'
+					},
+					{
+						id: 'schedule',
+						title: 'Schedule',
+						description: 'Send a recurring order, on a set schedule'
+					},
+					{
+						id: 'event',
+						title: 'Event Based',
+						description: 'Send a eCard based on an contacts event, birthday, anniversary, etc'
+					}
+				]}
+			/>
+		</div>
+		<div>
+			<DisplayTitle title="Set Timeline" />
+			<CheckCards
+				className="grid grid-cols-4"
+				type="radio"
+				hideCheck
+				passValue
+				groupName="sendSendTime"
+				options={[
+					{
+						id: 'now',
+						title: 'Send Now',
+						description: 'Send your eCard now',
+						checked: true
+					},
+					{
+						id: 'later',
+						title: 'Send Later',
+						description: 'Schedule your eCard for delivery'
+					}
+				]}
+			/>
+		</div>
+		<div>
+			<DisplayTitle title="Recipients" />
+			<CheckCards
+				className="grid grid-cols-4"
+				type="checkbox"
+				hideCheck
+				passValue
+				groupName="recipients"
+				options={data.contacts.map((c) => {
+					return {
+						id: c.id,
+						title: `${c.firstName} ${c.lastName}`,
+						description: `${c.email} | ${c.phone}`
+					};
+				})}
+			/>
+		</div>
+	</form>
 </Container>
